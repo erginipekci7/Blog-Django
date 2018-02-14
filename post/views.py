f2e31d3461ebf596da0dc0e2cf4e4f589abdae57
erginipekci7 <erginipekci7@gmail.com>
2018-02-15 01:40:31 +0300

    id ile detail sayfasına yönlendirme yapıldı

--- post/views.py
+++ post/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,HttpResponse
+from django.shortcuts import render, HttpResponse, get_object_or_404
 from .models import Post
 
 def post_index(request):
@@ -6,11 +6,17 @@ def post_index(request):
     return render(request, 'post/index.html', {'posts': posts})
     #return HttpResponse('post index sayfası')
 
-def post_detail(request):
-    return HttpResponse('post detail sayfası')
+def post_detail(request, id):
+    #post = Post.objects.get(id=2)
+    post = get_object_or_404(Post, id=id)
+    context = {
+        'post': post,
+    }
+    return render(request,'post/detail.html', context)
+    #return HttpResponse('post detail sayfası')
 def post_create(request):
     return HttpResponse('post create sayfası')
 def post_update(request):
     return HttpResponse('post update sayfası')
 def post_delete(request):
-    return HttpResponse(request)
+    return HttpResponse('post delete sayfası')
