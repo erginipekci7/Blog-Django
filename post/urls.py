681d8ed4d570b6cd9b5887038bd2699d38c39539
erginipekci7 <erginipekci7@gmail.com>
2018-02-15 01:38:04 +0300

    dinamik url yapısı olusturuldu

--- post/urls.py
+++ post/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url
 from .views import * #bütün view leri icine aktardık
+app_name = 'post'
 urlpatterns = [
 
-    url(r'^index/$', post_index),
-    url(r'^detail/$', post_detail),
-    url(r'^create/$', post_create),
-    url(r'^update/$', post_update),
-    url(r'^delete/$', post_delete),
+    url(r'^index/$', post_index, name='index'),
+    url(r'^(?P<id>\d+)$', post_detail, name='detail'),
+    url(r'^create/$', post_create, name='create'),
+    url(r'^update/$', post_update, name='update'),
+    url(r'^delete/$', post_delete, name='delete'),
 ]
